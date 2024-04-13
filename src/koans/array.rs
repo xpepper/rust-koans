c58d9f95402935dfb57de889ff1cbfda66ed733b
Pietro Di Bello <pierodibello@gmail.com>
2024-04-13 15:45:35 +0200

    first commit

--- src/koans/array.rs
+++ src/koans/array.rs
@@ -3,7 +3,7 @@
 #[test]
 fn array_index() {
     let arr: [i32; 5] = [1, 2, 3, 4, 5];
-    assert!(arr[__] == 1);
+    assert!(arr[0] == 1);
 }
 
 // A new fixed size array can be created by declaring the type of its elements
@@ -11,7 +11,7 @@ fn array_index() {
 // [i32; 0] = []
 #[test]
 fn array_empty() {
-    let arr: __;
+    let arr: [i32; 0] = [];
     assert!(arr.len() == 0);
 }
 
@@ -20,10 +20,10 @@ fn array_empty() {
 // that error in this example.
 #[test]
 #[should_panic]
-#[allow(const_err)]
+#[allow(unconditional_panic)]
 fn out_of_index() {
     let arr: [&'static str; 5] = ["rust", "is", "mostly", "for", "nerds"];
-    arr[__];
+    arr[6];
 }
 
 // Elements can be replaced in an array at a certain index.
@@ -31,7 +31,7 @@ fn out_of_index() {
 #[test]
 fn insert_at_index() {
     let mut arr: [u8; 5] = [0, 1, 2, 3, 4];
-    __ = 0;
+    arr[4] = 0;
     assert!(arr == [0, 1, 2, 3, 0]);
 }
 
@@ -40,16 +40,16 @@ fn insert_at_index() {
 fn array_iteration() {
     let arr: [u8; 3] = [3, 2, 1];
     let mut iterator = arr.iter();
-    assert!(iterator.next().unwrap() == &__);
-    assert!(iterator.next().unwrap() == &__);
-    assert!(iterator.next().unwrap() == &__);
+    assert!(iterator.next().unwrap() == &3);
+    assert!(iterator.next().unwrap() == &2);
+    assert!(iterator.next().unwrap() == &1);
 }
 
 // Arrays can also be mutated during iteration
 #[test]
 fn array_map() {
     let arr: [u32; 4] = [2, 5, 7, 4];
-    let mut iterator = arr.iter().map(__);
+    let mut iterator = arr.iter().map(|x| x * 2);
     assert!(iterator.next() == Some(4));
     assert!(iterator.next() == Some(10));
     assert!(iterator.next() == Some(14));
@@ -60,7 +60,7 @@ fn array_map() {
 #[test]
 fn array_filter() {
     let arr: [u16; 5] = [1, 2, 3, 4, 5];
-    let mut iterator = arr.iter().filter(__);
+    let mut iterator = arr.iter().filter(|x| *x % 2 == 0);
     assert!(iterator.next().unwrap() == &2);
     assert!(iterator.next().unwrap() == &4);
     assert!(iterator.next().is_none());
